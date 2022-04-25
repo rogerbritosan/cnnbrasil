1ed59d89efc9c06f6cd7a2b46ce359d5268d9120
Roger <roger@apis3.com>
2022-04-24 23:18:25 -0300

    fixing menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,22 @@ window.onload = () => {
     const mobileMenu = document.querySelector('.header__nav__mobile_button');
     const menuBox = document.querySelector('.header__nav__menu');
     const menuSearch = document.querySelector('.header__nav__search');
-    const menuBoxWrap = document.querySelector('.header__nav--wrap');
+    const menuBoxWrap = document.querySelector('.header__nav__menu--wrap');
+    const navMobile = document.querySelector('.header__nav__mobile');
+
     mobileMenu.addEventListener('click',()=>{
         if(mobileMenu.classList.contains('header__nav__mobile_button--active')) {
             mobileMenu.classList.remove('header__nav__mobile_button--active');
             menuBox.classList.remove('header__nav__menu--active');
             menuSearch.classList.remove('header__nav__search--active');
-            menuBoxWrap.classList.remove('header__nav--wrap--active');
+            menuBoxWrap.classList.remove('header__nav__menu--wrap--active');
+            navMobile.classList.remove('header__nav__mobile--active');
         } else {
             mobileMenu.classList.add('header__nav__mobile_button--active');
             menuBox.classList.add('header__nav__menu--active');
             menuSearch.classList.add('header__nav__search--active');
-            menuBoxWrap.classList.add('header__nav--wrap--active')
+            menuBoxWrap.classList.add('header__nav__menu--wrap--active')
+            navMobile.classList.add('header__nav__mobile--active');
         }
     });
 }
