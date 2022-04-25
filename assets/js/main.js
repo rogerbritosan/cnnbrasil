9819c73b313bd62ab87954335f6e2f7597f53b5c
Roger <roger@apis3.com>
2022-04-24 22:10:05 -0300

    fix menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,18 @@ window.onload = () => {
     const mobileMenu = document.querySelector('.header__nav__mobile_button');
     const menuBox = document.querySelector('.header__nav__menu');
     const menuSearch = document.querySelector('.header__nav__search');
+    const menuBoxWrap = document.querySelector('.header__nav--wrap');
     mobileMenu.addEventListener('click',()=>{
         if(mobileMenu.classList.contains('header__nav__mobile_button--active')) {
             mobileMenu.classList.remove('header__nav__mobile_button--active');
             menuBox.classList.remove('header__nav__menu--active');
             menuSearch.classList.remove('header__nav__search--active');
+            menuBoxWrap.classList.remove('header__nav--wrap--active');
         } else {
             mobileMenu.classList.add('header__nav__mobile_button--active');
             menuBox.classList.add('header__nav__menu--active');
             menuSearch.classList.add('header__nav__search--active');
+            menuBoxWrap.classList.add('header__nav--wrap--active')
         }
     });
 }
